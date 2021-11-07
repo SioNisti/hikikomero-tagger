dfd810764380cde90195aadebcb4880c895ef4bc
sionisti <qmenat@gmail.com>
2021-11-07 22:48:07 +0200

    assembly info

--- testiä/Properties/AssemblyInfo.cs
+++ testiä/Properties/AssemblyInfo.cs
@@ -4,13 +4,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("testiä")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("hikikomero-tagger")]
+[assembly: AssemblyDescription("stripped down version of hikikomero")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("testiä")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("sionTea")]
+[assembly: AssemblyProduct("hikikomero-tagger")]
+[assembly: AssemblyCopyright("sionTea")]
+[assembly: AssemblyTrademark("sionTea")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
